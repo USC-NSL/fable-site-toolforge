899c5d61ddc99ff8b5ec4d16561bcf35d21acd49
Nirosh Prakash <niroshprakash1995@gmail.com>
2024-03-29 16:40:26 -0700

    Data files

---
+++ fablesite/data/db_creation.sql
@@ -0,0 +1,13 @@
+create schema s55570__fable;
+
+use s55570__fable;
+
+CREATE TABLE aliases (
+  id INT NOT NULL AUTO_INCREMENT,
+  article TEXT,
+  link TEXT,
+  alias TEXT,
+  feedbackSelection TEXT,
+  feedbackInput TEXT,
+  PRIMARY KEY (id)
+)
---
+++ fablesite/data/loadData.py
@@ -0,0 +1,41 @@
+import pymysql
+import json
+
+# Load your JSON data
+with open("data_aliases.json", "r") as file:
+    data = json.load(file)
+
+# Database connection parameters
+connection_params = {
+    "host": "",
+    "user": "",
+    "password": "",
+    "db": "",
+    "cursorclass": pymysql.cursors.DictCursor,
+}
+
+# Connect to the database
+connection = pymysql.connect(**connection_params)
+
+try:
+    with connection.cursor() as cursor:
+        # SQL insert statement
+        sql = """
+        INSERT INTO aliases (article, link, alias)
+        VALUES (%s, %s, %s)
+        """
+
+        # Loop through the JSON data
+        for source, articles in data.items():
+            for article in articles:
+                # Prepare data for insertion
+                insert_data = (article["articleURL"], article["url"], article["alias"])
+                # Execute the SQL command
+                cursor.execute(sql, insert_data)
+
+        # Commit the changes
+        connection.commit()
+
+finally:
+    # Close the connection
+    connection.close()
